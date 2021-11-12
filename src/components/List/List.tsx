85d01fb942f1bacabfb915bb440b2c1fefc9d7af
Tyler Matteo <tylermatteo434@gmail.com>
2021-11-12 11:06:56 -0500

    Testing hook for type checking

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -3,12 +3,16 @@ export interface ListProps {
 }
 
 // This is a placeholder component to show a working unit test
-export const List = ({ items }: ListProps) => (
-  <ul>
-    {items.map((item, i) => (
-      <li key={i} data-testid="list-item">
-        {item}
-      </li>
-    ))}
-  </ul>
-);
+export const List = ({ items }: ListProps) => {
+  const foo: number = 'abc';
+  return (
+    <ul>
+      {items.map((item, i) => (
+        <li key={i} data-testid="list-item">
+          {item}
+          {foo}
+        </li>
+      ))}
+    </ul>
+  );
+};
