e23822927d4134526d6daffd29198c8fffa286f1
Tyler Matteo <tylermatteo434@gmail.com>
2021-11-12 11:13:36 -0500

    Add tsc to git hook with new pre-commit npm script and clean up List component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -3,14 +3,12 @@ export interface ListProps {
 }
 
 // This is a placeholder component to show a working unit test
-export const List = ({ items }: ListProps) => {
-  return (
-    <ul>
-      {items.map((item, i) => (
-        <li key={i} data-testid="list-item">
-          {item}
-        </li>
-      ))}
-    </ul>
-  );
-};
+export const List = ({ items }: ListProps) => (
+  <ul>
+    {items.map((item, i) => (
+      <li key={i} data-testid="list-item">
+        {item}
+      </li>
+    ))}
+  </ul>
+);
