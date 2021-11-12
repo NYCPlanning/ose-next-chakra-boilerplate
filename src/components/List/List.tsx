3848e58cd27d4182e745d23232b8e9668bea79d3
Tyler Matteo <tylermatteo434@gmail.com>
2021-11-12 11:07:25 -0500

    Remove type check violation

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -4,13 +4,11 @@ export interface ListProps {
 
 // This is a placeholder component to show a working unit test
 export const List = ({ items }: ListProps) => {
-  const foo: number = 'abc';
   return (
     <ul>
       {items.map((item, i) => (
         <li key={i} data-testid="list-item">
           {item}
-          {foo}
         </li>
       ))}
     </ul>
